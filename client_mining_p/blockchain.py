86569d7b2596100fba6dcef20c3f48d152051115
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-11 13:29:59 -0400

    Updates blockchain.py solution code

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -16,12 +16,11 @@ class Blockchain(object):
         self.current_transactions = []
         self.nodes = set()
 
-        self.new_block(previous_hash=1, proof=99)
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
-
         :param proof: <int> The proof given by the Proof of Work algorithm
         :param previous_hash: (Optional) <str> Hash of previous Block
         :return: <dict> New Block
@@ -44,7 +43,6 @@ class Blockchain(object):
     def new_transaction(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
-
         :param sender: <str> Address of the Recipient
         :param recipient: <str> Address of the Recipient
         :param amount: <int> Amount
@@ -63,7 +61,6 @@ class Blockchain(object):
     def hash(block):
         """
         Creates a SHA-256 hash of a Block
-
         :param block": <dict> Block
         "return": <str>
         """
@@ -78,19 +75,19 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    # def proof_of_work(self, last_proof):
-    #     """
-    #     Simple Proof of Work Algorithm
-    #     - Find a number p' such that hash(pp') contains 6 leading
-    #     zeroes, where p is the previous p'
-    #     - p is the previous proof, and p' is the new proof
-    #     """
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm
+        - Find a number p' such that hash(pp') contains 6 leading
+        zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        """
 
-    #     proof = 0
-    #     while self.valid_proof(last_proof, proof) is False:
-    #         proof += 1
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
-    #     return proof
+        return proof
 
     @staticmethod
     def valid_proof(last_proof, proof):
@@ -105,7 +102,6 @@ class Blockchain(object):
     def valid_chain(self, chain):
         """
         Determine if a given blockchain is valid
-
         :param chain: <list> A blockchain
         :return: <bool> True if valid, False if not
         """
@@ -131,6 +127,51 @@ class Blockchain(object):
 
         return True
 
+    # def register_node(self, address):
+    #     """
+    #     Add a new node to the list of nodes
+    #     :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+    #     :return: None
+    #     """
+
+    #     parsed_url = urlparse(address)
+    #     self.nodes.add(parsed_url.netloc)
+
+    # def resolve_conflicts(self):
+    #     """
+    #     This is our Consensus Algorithm, it resolves conflicts
+    #     by replacing our chain with the longest one in the network.
+    #     :return: <bool> True if our chain was replaced, False if not
+    #     """
+
+    #     neighbours = self.nodes
+    #     new_chain = None
+
+    #     # We're only looking for chains longer than ours
+    #     max_length = len(self.chain)
+
+    #     # Grab and verify the chains from all the nodes in our network
+    #     for node in neighbours:
+    #         response = requests.get(f'http://{node}/chain')
+
+    #         if response.status_code == 200:
+    #             length = response.json()['length']
+    #             chain = response.json()['chain']
+
+    #             # Check if the length is longer and the chain is valid
+    #             if length > max_length and self.valid_chain(chain):
+    #                 max_length = length
+    #                 new_chain = chain
+
+    #     # Replace our chain if we discovered a new, valid chain longer than
+    #     # ours
+    #     if new_chain:
+    #         self.chain = new_chain
+    #         return True
+
+    #     return False
+
+
 # Instantiate our Node
 app = Flask(__name__)
 
@@ -141,48 +182,42 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-# Change the method to accept a POST request
 @app.route('/mine', methods=['POST'])
 def mine():
     # Determine if proof is valid
     last_block = blockchain.last_block
     last_proof = last_block['proof']
-    # Get rid of line below b/c 'proof_of_work' was commented out above
-    # proof = blockchain.proof_of_work(last_proof)
 
     values = request.get_json()
-    
-    # Check that the required fields are in the POST'ed data
-    required = ['proof']
-    if not all(k in values for k in required):
-        return 'Missing Values', 400
+    submitted_proof = values.get('proof')
 
-    if not blockchain.valid_proof(last_proof, values['proof']):
+    if blockchain.valid_proof(last_proof, submitted_proof):
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mine a new coin
+        blockchain.new_transaction(
+            sender="0",
+            recipient=node_identifier,
+            amount=1,
+        )
+
+        # Forge the new BLock by adding it to the chain
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(submitted_proof, previous_hash)
+
         response = {
-            'message': 'Proof is invalid or already submitted'
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
         }
         return jsonify(response), 200
-        
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    else:
+        response = {
+            'message': "Proof was invalid or already submitted."
+        }
+        return jsonify(response), 200
 
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(values['proof'], previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
@@ -210,7 +245,7 @@ def full_chain():
     }
     return jsonify(response), 200
 
-# Endpoint that returns proof of the last block
+
 @app.route('/last_proof', methods=['GET'])
 def last_proof():
     last_proof_value = blockchain.last_block.get('proof')
@@ -219,11 +254,51 @@ def last_proof():
     }
     return jsonify(response), 200
 
+
+# # Post body as JSON to add node
+# # {
+# # 	"nodes": ["http://localhost:5001"]
+# # }
+# @app.route('/nodes/register', methods=['POST'])
+# def register_nodes():
+
+#     values = request.get_json()
+#     nodes = values.get('nodes')
+#     if nodes is None:
+#         return "Error: Please supply a valid list of nodes", 400
+
+#     for node in nodes:
+#         blockchain.register_node(node)
+
+#     response = {
+#         'message': 'New nodes have been added',
+#         'total_nodes': list(blockchain.nodes),
+#     }
+#     return jsonify(response), 201
+
+
+# @app.route('/nodes/resolve', methods=['GET'])
+# def consensus():
+#     replaced = blockchain.resolve_conflicts()
+
+#     if replaced:
+#         response = {
+#             'message': 'Our chain was replaced',
+#             'new_chain': blockchain.chain
+#         }
+#     else:
+#         response = {
+#             'message': 'Our chain is authoritative',
+#             'chain': blockchain.chain
+#         }
+
+#     return jsonify(response), 200
+
+
 # Note, when demoing, start with this, then change to the below
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
 
-# Python interpreter reads this source file and runs it.
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
